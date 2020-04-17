d6a4fcdcff315f98c02b4b7083361751bb774486
waldev <waldoaraque10@gmail.com>
2020-04-18 01:54:45 +0200

    adding views and rendering travels, creating a new model

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const Viaje = require('../models/Viajes')
 
 module.exports = () => {
     router.get('/', (req, res) => res.render('index')) 
 
     router.get('/nosotros', (req, res) => res.render('nosotros', {
         page: 'Sobre Nosotros'
+    }))
+
+    router.get('/viajes', (req, res) => Viaje.findAll()
+        .then(viajes => res.render('viajes', {
+            page: 'Próximos Viajes',
+            viajes 
+        }))
+        .catch(err => console.log(`Ha ocurrido un error: ${err}`))
+    )
+
+    router.get('/viajes/:id', (req, res) => Viaje.findById(req.params.id)
+        .then(viaje => res.render('viaje', {
+            viaje
+        }))
+        .catch(err => console.log(`Ha ocurrido un error: ${err}`))
+    )
+
+    router.get('/testimoniales', (req, res) => res.render('testimoniales', {
+        page: 'Testimoniales'
     }))
 
     return router
