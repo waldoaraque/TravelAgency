2b64e42c6615ff42d72e9cd3add43dc51adfa218
waldev <waldoaraque10@gmail.com>
2020-04-21 00:00:42 +0200

    converting to async/await the code in home.controller.js

--- server/controllers/home.controller.js
+++ server/controllers/home.controller.js
@@ -1,20 +1,18 @@
 const Viaje = require('../models/Viajes')
 const Testimonial = require('../models/Testimoniales')
 
-exports.homeController = (req, res) => {
-    const promises = []
-    promises.push(Viaje.findAll({ limit: 3 }))
-
-    promises.push(Testimonial.findAll({ limit: 3 }))
-
-    const result = Promise.all(promises)
+exports.homeController = async (req, res) => {
+    try {
+        const viajes = await Viaje.findAll({ limit: 3 })
+        const testimoniales = await Testimonial.findAll({ limit: 3 })
     
-    result
-        .then(rslt => res.render('index', {
+        res.render('index', {
             page: 'Próximos Viajes',
             clase: 'home',
-            viajes: rslt[0],
-            testimoniales: rslt[1] 
-        }))
-        .catch(err => console.log(`Ha ocurrido un error: ${err}`))  
+            viajes,
+            testimoniales
+        }) 
+    } catch (err) {
+        console.log(`Ha ocurrido un error: ${err}`)
+    }
 }
