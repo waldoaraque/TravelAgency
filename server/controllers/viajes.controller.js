a02bec71edac4275df00020c56c3692a58b09664
waldev <waldoaraque10@gmail.com>
2020-04-20 23:44:44 +0200

    converting my viajes.controller.js in async/await

--- server/controllers/viajes.controller.js
+++ server/controllers/viajes.controller.js
@@ -1,16 +1,22 @@
 const Viaje = require('../models/Viajes')
 
-exports.getViajes = (req, res) => {
-    Viaje.findAll()
-        .then(viajes => res.render('viajes', {
+exports.getViajes = async (req, res) => {
+    try {
+        const viajes = await Viaje.findAll()
+        res.render('viajes', {
             page: 'Próximos Viajes',
             viajes 
-        }))
-        .catch(err => console.log(`Ha ocurrido un error: ${err}`))
+        })
+    } catch (err) {
+        console.log(`Ha ocurrido un error: ${err}`)
+    }
 }
 
-exports.getViajesById = (req, res) => {
-    Viaje.findByPk(req.params.id)
-        .then(viaje => res.render('viaje', { viaje }))
-        .catch(err => console.log(`Ha ocurrido un error: ${err}`))
+exports.getViajesById = async (req, res) => {
+    try {
+        const viaje = await Viaje.findByPk(req.params.id)
+        res.render('viaje', { viaje })
+    } catch (err) {
+        console.log(`Ha ocurrido un error: ${err}`)
+    }    
 }
